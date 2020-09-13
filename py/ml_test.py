0fedc2ac54e09838342d874790448d32c0ef595d
周伟光 <zhouwg@eastcom-sw.com>
2020-09-13 10:24:07 +0800

    ci

--- py/ml_test.py
+++ py/ml_test.py
@@ -1,25 +1,27 @@
 import unittest
 import job_executor
 
+
 class MLTests(unittest.TestCase):
 
     def test_lr(self):
-        job_executor.execute("test_case_1_lr.json")
+        job_executor.execute("py/test_cases/test_case_1_lr.json")
 
     def test_dt(self):
-        job_executor.execute("test_case_2_decision_tree.json")
+        job_executor.execute("test_cases/test_case_2_decision_tree.json")
 
     def test_gdbt(self):
-        job_executor.execute("test_case_3_gdbt.json")
+        job_executor.execute("test_cases/test_case_3_gdbt.json")
 
     def test_csv(self):
-        job_executor.execute("test_case_4_csv.json")
+        job_executor.execute("test_cases/test_case_4_csv.json")
 
     def test_linear_regressor(self):
-        job_executor.execute("test_case_5_linear_regressor.json")
+        job_executor.execute("test_cases/test_case_5_linear_regressor.json")
 
     def test_elastic_net(self):
-        job_executor.execute("test_case_6_elastic_net.json")
+        job_executor.execute("test_cases/test_case_6_elastic_net.json")
+
 
 if __name__ == '__main__':
     unittest.main()
